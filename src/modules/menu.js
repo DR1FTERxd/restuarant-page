709ba2368e459779f6cc3b66a75ebce7b8dba7e4
DR1FTERxd <Eligijuskont@gmail.com>
2022-08-07 23:32:29 +0300

    all

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,15 +2,26 @@ function createMenu() {
    const menuTab = document.createElement('div')
    menuTab.setAttribute('id', 'menuTab')
    menuTab.setAttribute('data-tab-content', '#menu')
+   
    const menuTxt1 = document.createElement('h1')
    menuTxt1.setAttribute('id', 'menuTxt1')
-   menuTxt1.innerHTML= ('Menu')
+   menuTxt1.innerHTML= ('Our new special!')
+   
+   const menuPhoto1 = document.createElement('img');
+   menuPhoto1.setAttribute('id', 'MenuPhoto1');
+   menuPhoto1.src = ('/dist/images/menuphoto.png')
+
 
    const menuTxt2 = document.createElement('p')
    menuTxt2.setAttribute('id', 'menuTxt2')
    menuTxt2.innerHTML= ('our menu')
 
+   
+
+
+
    menuTab.appendChild(menuTxt1)
+   menuTab.appendChild(menuPhoto1)
    menuTab.appendChild(menuTxt2)
    return menuTab
 }
